9bb3c7fdf6e48fb452ad4648102f3b2631f28707
bmayuri20 <bmayuri100@gmail.com>
2024-10-21 19:08:00 +0530

    Finding the size of data types

---
+++ DataTypes.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class DataTypes{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       // int T = sc.nextInt();
+        //while (T-- > 0) {
+          //  sc.nextLine();
+            String s = sc.next();
+           Solution obj = new Solution();
+            int ans = obj.dataTypeSize(s);
+            String Character = "Character";
+            if(Character.equals(s) && ans==2){
+                ans = 1;
+            }
+            System.out.println(ans); 
+            sc.close();
+      //  }
+    }
+}
+class Solution{
+     int dataTypeSize(String str){
+        switch (str) {
+            case "Character":
+                return (Character.SIZE/8) ;
+                
+            case "Integer" :
+                return (Integer.SIZE/8) ;
+            
+            case "Long" :
+            return (Long.SIZE/8) ;
+
+            case "Float" :
+            return(Float.SIZE/8);
+
+            case "Double" :
+                 return (Double.SIZE/8);
+
+            default:
+            return -1;
+                
+        }
+
+    }
+}
