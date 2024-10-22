d6efb82e03b66c46f9a489513d7a8bd249c9256b
bmayuri20 <bmayuri100@gmail.com>
2024-10-23 00:07:35 +0530

    star pattern

---
+++ Pattern8.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/*
+Input Format: N = 3
+Result: 
+*****  
+ ***
+  *   
+ */
+public class Pattern8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N");
+        int n = sc.nextInt();
+        starPattern(n);
+        sc.close();
+    }
+
+    private static void starPattern(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j <= n - i - 1; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++)
+                System.out.print("*" + " ");
+            System.out.println();
+        }
+    }
+
+}
---
+++ Pattern9.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/*
+Input Format: N = 3
+Result: 
+  *  
+ ***
+***** 
+*****  
+ ***
+  *   
+ */
+public class Pattern9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N");
+        int n = sc.nextInt();
+        starPattern(n);
+        sc.close();
+    }
+
+    private static void starPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++)
+                System.out.print("*" + " ");
+            System.out.println();
+        }
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j <= n - i - 1; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++)
+                System.out.print("*" + " ");
+            System.out.println();
+        }
+    }
+
+}
